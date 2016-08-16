3b4b9f0b1929f80ce2e07931897b4e1a93f819f9
Dlugosz Tristan <tristand.pro@gmail.com>
2016-08-16 16:22:21 +0200

    modification graphique emacs

---
+++ init.el
@@ -0,0 +1,14 @@
+;;Environment
+(set-language-environment "UTF-8")
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(display-time-mode t)
+
+;; numéro de la colonne
+(column-number-mode t)
+
+(setq custom-file "~/.emacs.d/emacs-custom.el")
+(load custom-file) 
+
