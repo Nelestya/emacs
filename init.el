753869ca7569cab45a8a6b6d8f89d0ef648ba06d
Dlugosz Tristan <tristand.pro@gmail.com>
2016-08-18 03:27:22 +0200

    modif mineur

--- init.el
+++ init.el
@@ -7,7 +7,14 @@
 (display-time-mode t)
 
 ;; numéro de la colonne
+(global-linum-mode t)
 (column-number-mode t)
+
+;; afficher les espaces
+(global-whitespace-mode t)
+
+;;surligner la ligne du cureur
+(global-hl-line-mode t)
 
 (setq custom-file "~/.emacs.d/emacs-custom.el")
 (load custom-file) 
